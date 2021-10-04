b8e6454da2afedff10ae898de063bf08344c0f63
jtam1219 <jasontay526@gmail.com>
2021-10-04 16:42:11 -0400

    Final Commit, Added disabled buttons on load

--- src/components/EditToolbar.js
+++ src/components/EditToolbar.js
@@ -8,20 +8,20 @@ export default class EditToolbar extends React.Component {
             <div id="edit-toolbar">
                 <div 
                     id='undo-button' 
-                    className="top5-button"
+                    className="top5-button disabled"
                     onClick={doUndoCallback}>
                         &#x21B6;
                     
                 </div>
                 <div
                     id='redo-button'
-                    className="top5-button"
+                    className="top5-button disabled"
                     onClick={doRedoCallback}>
                         &#x21B7;
                 </div>
                 <div
                     id='close-button'
-                    className="top5-button"
+                    className="top5-button disabled"
                     onClick={closeListCallback}>
                         &#x24E7;
                 </div>
